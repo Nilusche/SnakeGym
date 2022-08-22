6c43c4a4d9b91091b2fe26f5b33d9efd43ce0518
Nilusche Liyanaarachchi <niluschel@outlook.de>
2022-08-22 09:18:20 +0200

    added dependencies

--- snake/setup.py
+++ snake/setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name = 'gymsnake',
       version = '0.1',
-      install_requires = ['gym'])
+      install_requires = ['gym', 'numpy', 'tensorflow', 'keras-rl2'])
