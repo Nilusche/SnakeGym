96ad8a955dc750aedcb25f07c5333d22bcbcba92
Nilusche Liyanaarachchi <niluschel@outlook.de>
2022-08-22 09:38:30 +0200

    added pillow dependency

--- snake/setup.py
+++ snake/setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name = 'gymsnake',
       version = '0.1',
-      install_requires = ['gym', 'numpy', 'tensorflow', 'keras-rl2'])
+      install_requires = ['gym', 'numpy', 'tensorflow', 'keras-rl2','pillow'])
